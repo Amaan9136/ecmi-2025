7906f4cf59a0f9f3deb4c9ee1149f59a52640f68
Amaan MK <syedkhalander66@gmail.com>
2025-02-27 18:25:36 +0530

    chore: Committee Correction

--- components/Committee/Index.jsx
+++ components/Committee/Index.jsx
@@ -136,7 +136,7 @@ const conferenceCommittee = {
 const conferenceCommittee2 = {
   nationalAdvisoryCommittee: [
     { name: "Dr. B E Rangaswamy", designation: "Registrar", institution: "VTU, Belagavi" },
-    { name: "Prof. S Y Kulkarni", designation: "Professor", institution: "Bengaluru" },
+    { name: "Prof. S Y Kulkarni", designation: "Director, BNMIT, Bengaluru", institution: "Advisor, ACU, BG Nagara, Mandya" },
     { name: "Dr. E S Chakravarthy", designation: "Advisor", institution: "ACU, BG Nagara, Mandya" },
     { name: "Dr. Karunakara Rai", designation: "Professor", institution: "NMIT, Bengaluru" },
     { name: "Dr. Puttamadappa", designation: "Registrar", institution: "Dayananda University, Bengaluru" },
@@ -152,22 +152,113 @@ const conferenceCommittee2 = {
     { name: "Dr. Naveen B", designation: "Associate Professor, Dept. of ECE", institution: "ACU, BG Nagara, Mandya" }
   ],
   technicalProgramCommittee: [
-    { name: "Prof. N. Shekar V Shet", designation: "Professor and Head, Dept. of ECE", institution: "NITK, Surathkal" },
-    { name: "Dr. B Surendiran", designation: "Professor, Dept. of CSE", institution: "National Institute of Technology (NIT), Puducherry, Thiruvettakudy, Karaikal" },
-    { name: "Dr. Geetha V", designation: "Associate Professor, Dept. of Information Technology", institution: "NITK, Surathkal" },
-    { name: "Dr. Viswanath Talasila", designation: "Professor and Head, ETE", institution: "MSRIT, Bengaluru" },
-    { name: "Dr. Paritosh Peshwe", designation: "Professor", institution: "IIIT Nagpur" },
-    { name: "Dr. Sunil C K", designation: "Assistant Professor, Dept. of Computer Science and Engineering", institution: "Indian Institute of Information Technology, Dharwad" },
-    { name: "Dr. S G Shivaprasad Yadav", designation: "Former Honorary Secretary, IETE Bengaluru, Associate Professor, Dept. of ETE", institution: "Ramaiah Institute of Technology, Bengaluru" },
-    { name: "Dr. Arvind Kumar G", designation: "Professor, ETE", institution: "MSRIT, Bengaluru" },
-    { name: "Prof. K C B Rao", designation: "Professor", institution: "University College of Engineering, Vizianagaram, JNTUK" },
-    { name: "Dr. A P Jagadeesh Chandra", designation: "Professor, Dept. of ECE", institution: "AIT, Chikkamagaluru" },
-    { name: "Dr. Madhu Patil", designation: "Professor & HOD, Dept. of Electrical Science & Engineering", institution: "BGSCET, Bengaluru" },
-    { name: "Dr. Swetha Amit", designation: "Senior Application Engineer", institution: "Entuple Technology Pvt Ltd, Bengaluru" },
-    { name: "Dr. Ranjitha U N", designation: "Associate Professor and Head, CSE, School of Computer Science and Engineering", institution: "REVA University, Bengaluru" },
-    { name: "Dr. Maithri C", designation: "Professor and Head, Dept. of CSE", institution: "Kalpataru Institute of Technology, Tiptur" },
-    { name: "Dr. Rajesh I S", designation: "Assistant Professor, Dept. of AI&ML", institution: "BMSIT, Bengaluru" }
-  ],
+    { 
+        name: "Prof. N Shekar V Shet", 
+        designation: "Professor and Head, Dept. of ECE", 
+        institution: "NITK, Surathkal" 
+    },
+    { 
+        name: "Dr. B Surendiran", 
+        designation: "Professor, Dept. of CSE", 
+        institution: "NIT, Puducherry" 
+    },
+    { 
+        name: "Dr. Geetha V", 
+        designation: "Associate Professor, Dept. of Information Technology", 
+        institution: "NITK, Surathkal" 
+    },
+    { 
+        name: "Dr. Viswanath Talasila", 
+        designation: "Professor and Head, Dept. of ETE", 
+        institution: "MSRIT, Bengaluru" 
+    },
+    { 
+        name: "Dr. Paritosh Peshwe", 
+        designation: "Professor, Dept. of ECE", 
+        institution: "IIIT, Nagpur" 
+    },
+    { 
+        name: "Dr. Mandeep Singh", 
+        designation: "Visiting Scientist, IISc Bangalore, Assistant Professor, Dept. of E&C", 
+        institution: "NITK, Surathkal" 
+    },
+    { 
+        name: "Dr. C P Ravikumar", 
+        designation: "Director of Technical Talent Development", 
+        institution: "Texas Instruments, India" 
+    },
+    { 
+        name: "Dr. Sunil C K", 
+        designation: "Assistant Professor, Dept. of CSE", 
+        institution: "IIIT, Dharwad" 
+    },
+    { 
+        name: "Dr. S G Shivaprasad Yadav", 
+        designation: "Former Honorary Secretary, IETE Bengaluru, Associate Professor, Dept. of ETE", 
+        institution: "MSRIT, Bengaluru" 
+    },
+    { 
+        name: "Dr. Arvind Kumar G", 
+        designation: "Professor, ETE", 
+        institution: "MSRIT, Bengaluru" 
+    },
+    { 
+        name: "Prof. K C B Rao", 
+        designation: "Professor", 
+        institution: "University College of Engineering, Vizianagaram, JNTUK" 
+    },
+    { 
+        name: "Dr. A P Jagadeesh Chandra", 
+        designation: "Professor, Dept. of ECE", 
+        institution: "AIT, Chikkamagaluru" 
+    },
+    { 
+        name: "Dr. Madhu Patil", 
+        designation: "Professor & HOD, Dept. of Electrical Science & Engineering", 
+        institution: "BGSCET, Bengaluru" 
+    },
+    { 
+        name: "Dr. P C Shrikanth", 
+        designation: "Professor & Head, Dept. of ECE", 
+        institution: "MCE, Hassan" 
+    },
+    { 
+        name: "Dr. K Sreelakshmi", 
+        designation: "Professor & PG Dean Studies", 
+        institution: "RVCE, Bengaluru" 
+    },
+    { 
+        name: "Dr. Basavaraj Jagadale", 
+        designation: "Professor & Chairman, Dept. of PG Studies & Research in Electronics", 
+        institution: "Kuvempu University, Shankaraghatta" 
+    },
+    { 
+        name: "Dr. S Ravishankar", 
+        designation: "Visiting Professor, Dept. of ECE", 
+        institution: "RVCE, Bengaluru" 
+    },
+    { 
+        name: "Dr. Swetha Amit", 
+        designation: "Senior Application Engineer", 
+        institution: "Entuple Technology Pvt Ltd, Bengaluru" 
+    },
+    { 
+        name: "Dr. Ranjitha U N", 
+        designation: "Associate Professor and Head, CSE, School of Computer Science and Engineering", 
+        institution: "REVA University, Bengaluru" 
+    },
+    { 
+        name: "Dr. Maithri C", 
+        designation: "Professor and Head, Dept. of CSE", 
+        institution: "Kalpataru Institute of Technology, Tiptur" 
+    },
+    { 
+        name: "Dr. Rajesh I S", 
+        designation: "Assistant Professor, Dept. of AI&ML", 
+        institution: "BMSIT, Bengaluru" 
+    }
+]
+
 }
 
 const conferenceCommittee3 = {
@@ -204,6 +295,11 @@ const conferenceCommittee3 = {
     {
       name: "Dr. Hong Li",
       institution: "University of Houston-Downtown, USA"
+    },
+    {
+      name: "Dr. Yuvaraja Teekaraman",
+      designation: "Professor of Electrical Engineering",
+      institution: "School of Engineering & Computing American International University, Kuwait."
     }
   ],
 };
